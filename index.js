7412aca0c576699d4a4c3ae6978d78f4c73409fc
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2024-10-02 20:36:40 -0700

    appending tags to container

--- index.js
+++ index.js
@@ -4,19 +4,30 @@ const containerDiv = document.getElementById('container');
 const fetchData = () => {
     fetch(desserts_API)
         .then(response => response.json())
-        .then(desserts => console.log(desserts));
+        .then(desserts => renderDesserts(desserts));
     
 }
 
-const renderDesserts = desserts => {
-    desserts.forEach(dessert => {
+function renderDesserts(desserts) {
+    desserts.forEach((dessert, i) => {
         const div = document.createElement('div');
+        const image = document.createElement('img');
         const button = document.createElement('button');
         const name = document.createElement('p');
         const category = document.createElement('h3');
         const price = document.createElement('p');
+
+        containerDiv.classList.add('flex-container')
+
+        div.classList.add('card');
+        image.src = dessert.image.desktop;
+
+        containerDiv.appendChild(div)
+        div.appendChild(image)
     })
 }
 
 
 fetchData();
+
+renderDesserts

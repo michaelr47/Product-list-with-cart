f244a48e210ec29406695d64f4ab3631fe437e18
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2024-10-06 22:35:27 -0700

    rendering func in progress

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const fetchData = () => {
 }
 
 function renderDesserts(desserts) {
+    containerDiv.classList.add('flex-container')
+
     desserts.forEach((dessert, i) => {
         const div = document.createElement('div');
         const image = document.createElement('img');
@@ -17,17 +19,24 @@ function renderDesserts(desserts) {
         const category = document.createElement('h3');
         const price = document.createElement('p');
 
-        containerDiv.classList.add('flex-container')
-
-        div.classList.add('card');
         image.src = dessert.image.desktop;
+        name.textContent = dessert.name; 
+        category.textContent = dessert.category; 
+        price.textContent = `$${dessert.price}`; 
+        button.textContent = 'Add to Cart';
 
-        containerDiv.appendChild(div)
         div.appendChild(image)
+        div.appendChild(name);
+        div.appendChild(category);
+        div.appendChild(price);
+        div.appendChild(button);
+        div.classList.add('card');
+        
+        containerDiv.appendChild(div)
+        
     })
 }
 
 
 fetchData();
 
-renderDesserts

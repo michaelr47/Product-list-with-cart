d03d938f47ebad473c87ed0d54c6a6513dd345a6
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2024-10-21 20:52:59 -0700

    fix overlay of content, starting cart section

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const fetchData = () => {
 }
 
 function renderDesserts(desserts) {
-    containerDiv.classList.add('flex-container')
+    containerDiv.setAttribute('id', 'container');
 
     desserts.forEach((dessert, i) => {
         const div = document.createElement('div');
@@ -18,7 +18,7 @@ function renderDesserts(desserts) {
         const name = document.createElement('p');
         const category = document.createElement('h3');
         const price = document.createElement('p');
-
+ 
         image.src = dessert.image.desktop;
         name.textContent = dessert.name; 
         category.textContent = dessert.category; 
